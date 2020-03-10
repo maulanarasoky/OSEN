bf3307fc7e68a5d738ebef803f934e882559bff9
maulanarasoky <rasoky.maulana@gmail.com>
2020-03-11 05:35:24 +0700

    Make a Register Function

--- app/src/main/java/com/example/osen/activity/Register.kt
+++ app/src/main/java/com/example/osen/activity/Register.kt
@@ -3,11 +3,48 @@ package com.example.osen.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.osen.R
+import com.example.osen.model.data.User
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
 
 class Register : AppCompatActivity() {
+    lateinit var user: User
+    lateinit var database: DatabaseReference
+
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        database = FirebaseDatabase.getInstance().reference.child("Account")
+        database.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    count = p0.childrenCount.toInt()
+                }
+            }
+
+        })
+
+        btnRegister.setOnClickListener {
+            if(password.text.toString() == reTypePass.text.toString()){
+                user = User(shuffle(), username.text.toString(), password.text.toString())
+
+                database.child((count + 1).toString()).setValue(user)
+                toast("Registrasi berhasil").show()
+            }
+        }
+    }
+
+    private fun shuffle(): String{
+        val allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz1234567890"
+        return (1..10)
+            .map { allowedChars.random() }
+            .joinToString("")
     }
 }
---
+++ app/src/main/java/com/example/osen/model/data/User.kt
@@ -0,0 +1,3 @@
+package com.example.osen.model.data
+
+class User(val id: String, val username: String, val password: String)
