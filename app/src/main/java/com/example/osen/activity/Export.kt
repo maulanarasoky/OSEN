2442096da30f5511af2d9cf95ebf9858e9918edf
maulanarasoky <rasoky.maulana@gmail.com>
2020-03-13 18:24:44 +0700

    Designing Layout for Export

---
+++ app/src/main/java/com/example/osen/activity/Export.kt
@@ -0,0 +1,13 @@
+package com.example.osen.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.osen.R
+
+class Export : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_export)
+    }
+}
--- app/src/main/java/com/example/osen/activity/ExportImport.kt
+++ app/src/main/java/com/example/osen/activity/ExportImport.kt
@@ -3,11 +3,21 @@ package com.example.osen.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.osen.R
+import kotlinx.android.synthetic.main.activity_export_import.*
+import org.jetbrains.anko.startActivity
 
 class ExportImport : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_export_import)
+
+        linear1.setOnClickListener {
+            startActivity<Export>()
+        }
+
+        linear2.setOnClickListener {
+
+        }
     }
 }
