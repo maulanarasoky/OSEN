fc33a79c5ba8fdb818cdcae46e068fe0bfd824cf
maulanarasoky <rasoky.maulana@gmail.com>
2020-03-18 15:59:58 +0700

    Make an Alarm Manager for Auto Backup

--- app/src/main/java/com/example/osen/activity/Backup.kt
+++ app/src/main/java/com/example/osen/activity/Backup.kt
@@ -1,54 +1,38 @@
 package com.example.osen.activity
 
-import android.content.Context
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import com.example.osen.R
-import com.example.osen.model.*
+import com.example.osen.alarm.AutoBackup
 import com.example.osen.preference.BackupPreference
 import com.example.osen.preference.DataPreference
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_backup.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.File
-import java.io.IOException
-import java.io.OutputStreamWriter
 
 class Backup : AppCompatActivity() {
 
-    lateinit var auth: FirebaseAuth
-    lateinit var storage: StorageReference
-
     var dataPreference = DataPreference()
     lateinit var backupPreference: BackupPreference
+
+    lateinit var alarmManager: AutoBackup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_backup)
 
-        auth = FirebaseAuth.getInstance()
-        storage = FirebaseStorage.getInstance().reference
         backupPreference = BackupPreference(this)
+        alarmManager = AutoBackup()
 
         showExistingPreference()
 
         backupClasses.setOnClickListener {
             if(!dataPreference.osen_classes){
                 dataPreference.osen_classes = true
+                alarmManager.setAutoBackup(this, AutoBackup.TYPE_BACKUP_CLASSES, AutoBackup.ID_BACKUP_CLASSES)
                 Toast.makeText(this, "Auto Backup kelas dinyalakan", Toast.LENGTH_SHORT).show()
-//                val fileName = "Osen_Classes.json"
-//                export(this, Classroom.TABLE_CLASSROOM, fileName, "Kelas")
             }else{
                 dataPreference.osen_classes = false
+                alarmManager.cancelAutoBackup(this, AutoBackup.ID_BACKUP_CLASSES)
                 Toast.makeText(this, "Auto Backup kelas dimatikan", Toast.LENGTH_SHORT).show()
             }
             backupPreference.setClasses(dataPreference)
@@ -56,106 +40,50 @@ class Backup : AppCompatActivity() {
         backupStudents.setOnClickListener {
             if(!dataPreference.osen_students){
                 dataPreference.osen_students = true
+                alarmManager.setAutoBackup(this, AutoBackup.TYPE_BACKUP_STUDENTS, AutoBackup.ID_BACKUP_STUDENTS)
                 Toast.makeText(this, "Auto Backup murid dinyalakan", Toast.LENGTH_SHORT).show()
-//                val fileName = "Osen_Classes.json"
-//                export(this, Classroom.TABLE_CLASSROOM, fileName, "Kelas")
             }else{
                 dataPreference.osen_students = false
+                alarmManager.cancelAutoBackup(this, AutoBackup.ID_BACKUP_STUDENTS)
                 Toast.makeText(this, "Auto Backup murid dimatikan", Toast.LENGTH_SHORT).show()
             }
             backupPreference.setStudents(dataPreference)
-//            val fileName = "Osen_Students.json"
-//            export(this, Student.TABLE_STUDENT, fileName, "Murid")
         }
         backupAbsents.setOnClickListener {
             if(!dataPreference.osen_absents){
                 dataPreference.osen_absents = true
+                alarmManager.setAutoBackup(this, AutoBackup.TYPE_BACKUP_ABSENTS, AutoBackup.ID_BACKUP_ABSENTS)
                 Toast.makeText(this, "Auto Backup absen dinyalakan", Toast.LENGTH_SHORT).show()
-//                val fileName = "Osen_Classes.json"
-//                export(this, Classroom.TABLE_CLASSROOM, fileName, "Kelas")
             }else{
                 dataPreference.osen_absents = false
+                alarmManager.cancelAutoBackup(this, AutoBackup.ID_BACKUP_ABSENTS)
                 Toast.makeText(this, "Auto Backup absen dimatikan", Toast.LENGTH_SHORT).show()
             }
             backupPreference.setAbsents(dataPreference)
-//            val fileName = "Osen_Absents.json"
-//            export(this, Absent.TABLE_ABSENT, fileName, "Absen")
         }
         backupScores.setOnClickListener {
             if(!dataPreference.osen_scores){
                 dataPreference.osen_scores = true
+                alarmManager.setAutoBackup(this, AutoBackup.TYPE_BACKUP_SCORES, AutoBackup.ID_BACKUP_SCORES)
                 Toast.makeText(this, "Auto Backup nilai dinyalakan", Toast.LENGTH_SHORT).show()
-//                val fileName = "Osen_Classes.json"
-//                export(this, Classroom.TABLE_CLASSROOM, fileName, "Kelas")
             }else{
                 dataPreference.osen_scores = false
+                alarmManager.cancelAutoBackup(this, AutoBackup.ID_BACKUP_SCORES)
                 Toast.makeText(this, "Auto Backup nilai dimatikan", Toast.LENGTH_SHORT).show()
             }
             backupPreference.setScores(dataPreference)
-//            val fileName = "Osen_Scores.json"
-//            export(this, Score.TABLE_SCORE, fileName, "Nilai")
         }
         backupCategories.setOnClickListener {
             if(!dataPreference.osen_categories){
                 dataPreference.osen_categories = true
+                alarmManager.setAutoBackup(this, AutoBackup.TYPE_BACKUP_CATEGORIES, AutoBackup.ID_BACKUP_CATEGORIES)
                 Toast.makeText(this, "Auto Backup kategori dinyalakan", Toast.LENGTH_SHORT).show()
-//                val fileName = "Osen_Classes.json"
-//                export(this, Classroom.TABLE_CLASSROOM, fileName, "Kelas")
             }else{
                 dataPreference.osen_categories = false
+                alarmManager.cancelAutoBackup(this, AutoBackup.ID_BACKUP_CATEGORIES)
                 Toast.makeText(this, "Auto Backup kategori dimatikan", Toast.LENGTH_SHORT).show()
             }
             backupPreference.setCategories(dataPreference)
-//            val fileName = "Osen_Categories.json"
-//            export(this, Category.TABLE_CATEGORY, fileName, "Kategori")
-        }
-    }
-
-    private fun getResults(myTable: String): JSONArray? {
-        val myPath: String = getDatabasePath("Osen.db").toString()
-        val searchQuery = "SELECT  * FROM $myTable WHERE TEACHER_ID = '${auth.currentUser?.uid}'"
-        val myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY)
-        val cursor: Cursor = myDataBase.rawQuery(searchQuery, null)
-        val resultSet = JSONArray()
-        cursor.moveToFirst()
-        while (!cursor.isAfterLast) {
-            val totalColumn: Int = cursor.columnCount
-            val rowObject = JSONObject()
-            for (i in 0 until totalColumn) {
-                if (cursor.getColumnName(i) != null) {
-                    try {
-                        if (cursor.getString(i) != null) {
-                            rowObject.put(cursor.getColumnName(i), cursor.getString(i))
-                        } else {
-                            rowObject.put(cursor.getColumnName(i), "")
-                        }
-                    } catch (e: Exception) {
-                        Log.d("TAG_NAME", e.message.toString())
-                    }
-                }
-            }
-            resultSet.put(rowObject)
-            cursor.moveToNext()
-        }
-        cursor.close()
-        Log.d("TAG_NAME", resultSet.toString())
-        return resultSet
-    }
-
-    private fun export(context: Context, myTable: String, fileName: String, description: String) {
-        try {
-            val outputStreamWriter =
-                OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
-            outputStreamWriter.write(getResults(myTable).toString())
-            outputStreamWriter.close()
-
-            val fileLocation: File = File(filesDir, fileName)
-            val path: Uri =
-                FileProvider.getUriForFile(this, "com.example.osen.fileprovider", fileLocation)
-            val folder = storage.child("${auth.currentUser?.email}/${fileName}")
-            folder.putFile(path)
-        } catch (e: IOException) {
-            Log.e("ERROR", "File write failed :", e)
         }
     }
 
---
+++ app/src/main/java/com/example/osen/alarm/AutoBackup.kt
@@ -0,0 +1,145 @@
+package com.example.osen.alarm
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.net.Uri
+import android.util.Log
+import androidx.core.content.FileProvider
+import com.example.osen.model.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.util.*
+
+class AutoBackup: BroadcastReceiver() {
+
+    companion object{
+        const val TYPE_BACKUP_CLASSES = "TYPE_BACKUP_CLASSES"
+        const val TYPE_BACKUP_STUDENTS = "TYPE_BACKUP_STUDENTS"
+        const val TYPE_BACKUP_ABSENTS = "TYPE_BACKUP_ABSENTS"
+        const val TYPE_BACKUP_SCORES = "TYPE_BACKUP_SCORES"
+        const val TYPE_BACKUP_CATEGORIES = "TYPE_BACKUP_CATEGORIES"
+
+        const val ID_BACKUP_CLASSES = 101
+        const val ID_BACKUP_STUDENTS = 102
+        const val ID_BACKUP_ABSENTS = 103
+        const val ID_BACKUP_SCORES = 104
+        const val ID_BACKUP_CATEGORIES = 105
+    }
+
+    val EXTRA_TYPE = "EXTRA_TYPE"
+
+    lateinit var storage: StorageReference
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(context != null){
+            auth = FirebaseAuth.getInstance()
+            storage = FirebaseStorage.getInstance().reference
+            val type = intent?.getStringExtra(EXTRA_TYPE)
+            when(type){
+                TYPE_BACKUP_CLASSES -> {
+                    export(context, Classroom.TABLE_CLASSROOM, "Osen_Classes.json")
+                    Log.d("BACKUP", "BACKUP AKTIF")
+                }
+                TYPE_BACKUP_STUDENTS -> {
+                    export(context, Student.TABLE_STUDENT, "Osen_Students.json")
+                }
+                TYPE_BACKUP_ABSENTS -> {
+                    export(context, Absent.TABLE_ABSENT, "Osen_Absents.json")
+                }
+                TYPE_BACKUP_SCORES -> {
+                    export(context, Score.TABLE_SCORE, "Osen_Scores.json")
+                }
+                TYPE_BACKUP_CATEGORIES -> {
+                    export(context, Category.TABLE_CATEGORY, "Osen_Categories.json")
+                }
+                else -> {
+                    Log.d("ERROR", "ERROR BACK UP")
+                }
+            }
+        }
+    }
+
+    fun setAutoBackup(context: Context, type: String, id: Int){
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        val intent = Intent(context, AutoBackup::class.java)
+        intent.putExtra(EXTRA_TYPE, type)
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 7)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, id, intent, 0)
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+    }
+
+    fun cancelAutoBackup(context: Context, id: Int){
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AutoBackup::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, id, intent, 0)
+        pendingIntent.cancel()
+        alarmManager.cancel(pendingIntent)
+    }
+
+    private fun getResults(context: Context, myTable: String): JSONArray? {
+        val myPath: String = context.getDatabasePath("Osen.db").toString()
+        val searchQuery = "SELECT  * FROM $myTable WHERE TEACHER_ID = '${auth.currentUser?.uid}'"
+        val myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY)
+        val cursor: Cursor = myDataBase.rawQuery(searchQuery, null)
+        val resultSet = JSONArray()
+        cursor.moveToFirst()
+        while (!cursor.isAfterLast) {
+            val totalColumn: Int = cursor.columnCount
+            val rowObject = JSONObject()
+            for (i in 0 until totalColumn) {
+                if (cursor.getColumnName(i) != null) {
+                    try {
+                        if (cursor.getString(i) != null) {
+                            rowObject.put(cursor.getColumnName(i), cursor.getString(i))
+                        } else {
+                            rowObject.put(cursor.getColumnName(i), "")
+                        }
+                    } catch (e: Exception) {
+                        Log.d("TAG_NAME", e.message.toString())
+                    }
+                }
+            }
+            resultSet.put(rowObject)
+            cursor.moveToNext()
+        }
+        cursor.close()
+        Log.d("TAG_NAME", resultSet.toString())
+        return resultSet
+    }
+
+    private fun export(context: Context, myTable: String, fileName: String) {
+        try {
+            val outputStreamWriter =
+                OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
+            outputStreamWriter.write(getResults(context, myTable).toString())
+            outputStreamWriter.close()
+
+            val fileLocation = File(context.filesDir, fileName)
+            val path: Uri =
+                FileProvider.getUriForFile(context, "com.example.osen.fileprovider", fileLocation)
+            val folder = storage.child("${auth.currentUser?.email}/${fileName}")
+            folder.putFile(path)
+        } catch (e: IOException) {
+            Log.e("ERROR", "File write failed :", e)
+        }
+    }
+}
